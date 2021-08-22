98adf78ed038ec2a18fe2d7b25934ff957e42f44
Arthur Tan <85665870+arthurtansoongshin@users.noreply.github.com>
2021-08-22 16:51:52 +0800

    Added headers for GET requests and optional sorting functionalities.

--- pba.py
+++ pba.py
@@ -4,39 +4,39 @@ import csv
 import os
 
 
-def loadPage(url):
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, "lxml")
-    return soup
-
-
-def saveCSV(filename, data):
-    os.makedirs(os.path.abspath("out"), exist_ok=True)
-    with open(f"out/{filename}.csv", "w", newline="", encoding="UTF-8") as file:
-        writer = csv.writer(file)
-        for data_arr in data:
-            writer.writerow(data_arr)
-
-
-class TeamInfoExtract:
+class InfoExtract:
     def __init__(self, main_page_url):
+        self.headers = {
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "accept-language": "en-US,en;q=0.9",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36",
+        }
         self.main_page_url = main_page_url
-        self.team_urls = []
-        self.team_img_urls = []
-        self.team_names = []
-        self.team_coaches = []
-        self.team_managers = []
-        self.soup = loadPage(main_page_url)
+        self.soup = InfoExtract.loadPage(main_page_url, self.headers)
 
-    def getImgUrls(self):
-        els = self.soup.select(".team-page-team-logos")
-        for el in els:
-            self.team_img_urls.append(el["src"])
+    def loadPage(url, headers):
+        page = requests.get(url, headers=headers)
+        soup = BeautifulSoup(page.content, "lxml")
+        return soup
 
-    def getLogos(self):
+    def saveCSV(filename, data):
+        os.makedirs(os.path.abspath("out"), exist_ok=True)
+        with open(f"out/{filename}.csv", "w", newline="", encoding="UTF-8") as file:
+            writer = csv.writer(file)
+            for data_arr in data:
+                writer.writerow(data_arr)
+
+
+class TeamInfoExtract(InfoExtract):
+    def getImgUrl(self):
+        team_img_url_els = self.soup.select(".team-page-team-logos")
+        self.team_img_urls = list(map(lambda n: n["src"], team_img_url_els))
+
+    def getLogo(self):
+        self.getImgUrl()
         for url in self.team_img_urls:
             filename = url.split("/")[-1]
-            res = requests.get(url)
+            res = requests.get(url, headers=self.headers)
             os.makedirs(os.path.abspath("logos"), exist_ok=True)
             if res.status_code == 200:
                 with open(f"logos/{filename}", "wb") as file:
@@ -44,12 +44,15 @@ class TeamInfoExtract:
 
     def getUrl(self):
         team_url_els = self.soup.select(".team-page-img a")
-        for el in team_url_els:
-            self.team_urls.append(el["href"])
+        self.team_urls = list(map(lambda n: n["href"], team_url_els))
 
     def getInfo(self):
+        self.team_names = []
+        self.team_coaches = []
+        self.team_managers = []
+        self.getUrl()
         for url in self.team_urls:
-            self.soup = loadPage(url)
+            self.soup = InfoExtract.loadPage(url, self.headers)
             team_name_el = self.soup.select_one(".team-profile-data h3")
             self.team_coach_manager_el = self.soup.select(".team-personal-bar .team-mgmt-data")
             self.team_names.append(team_name_el.get_text())
@@ -57,24 +60,17 @@ class TeamInfoExtract:
             self.team_managers.append(self.team_coach_manager_el[1].get_text())
 
     def saveToCSV(self):
-        teams_data = [["url", "image", "name", "coach", "manager"]]
+        teams_data = []
         for i in range(len(self.team_urls)):
             teams_data.append([self.team_urls[i], self.team_img_urls[i], self.team_names[i], self.team_coaches[i], self.team_managers[i]])
             i += 1
-        saveCSV("teams", teams_data)
+        # Use this to sort by team names
+        # teams_data.sort(key=lambda l: l[2])
+        teams_data.insert(0, ["url", "image", "name", "coach", "manager"])
+        InfoExtract.saveCSV("teams", teams_data)
 
 
-class PlayerInfoExtract:
-    def __init__(self, main_page_url):
-        self.main_page_url = main_page_url
-        self.pl_names = []
-        self.pl_teams = []
-        self.pl_nums = []
-        self.pl_positions = []
-        self.pl_urls = []
-        self.pl_mugshots = []
-        self.soup = loadPage(main_page_url)
-
+class PlayerInfoExtract(InfoExtract):
     def getInfo(self, team_ref):
         pl_name_els = self.soup.select(".playersBox a[href] > h5")
         pl_team_url_els = self.soup.select(".playersBox span > img")
@@ -89,19 +85,20 @@ class PlayerInfoExtract:
         self.pl_mugshots = list(map(lambda n: n["src"], pl_mugshot_els))
 
     def saveToCSV(self):
-        players_data = [["team", "name", "number", "position", "url", "mugshot"]]
+        players_data = []
         for i in range(len(self.pl_names)):
             players_data.append([self.pl_teams[i], self.pl_names[i], self.pl_nums[i], self.pl_positions[i], self.pl_urls[i], self.pl_mugshots[i]])
             i += 1
-        saveCSV("players", players_data)
+        # Use this to sort by player names
+        # players_data.sort(key=lambda l: l[1])
+        players_data.insert(0, ["team", "name", "number", "position", "url", "mugshot"])
+        InfoExtract.saveCSV("players", players_data)
 
 
 try:
     print("Extracting teams info and downloading logos...")
     team_info_extract = TeamInfoExtract("https://www.pba.ph/teams")
-    team_info_extract.getImgUrls()
-    team_info_extract.getLogos()
-    team_info_extract.getUrl()
+    team_info_extract.getLogo()
     team_info_extract.getInfo()
     team_info_extract.saveToCSV()
     print("Done extracting teams info and downloading logos.")
